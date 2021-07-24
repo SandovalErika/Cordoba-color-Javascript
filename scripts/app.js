2a1c54ad45f467e9195dbd48ddbb1473e67f6642
Erika Sandoval <erikasandoval-09@hotmail.com>
2021-07-24 15:06:28 -0300

    1era entrega del proyecto final

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+let bienvenido = prompt ("¡Bienvenido a CORDOBA COLOR! ¿Cual es tu nombre?")
+let pregunta= prompt ("¿Qué producto buscas?")
+
+function ayuda() {
+    alert("Hola " + bienvenido  + ". Tenemos "  + pregunta + " a tu disposición" + ". Dirigite a seccion PRODUCTOS para mas información")
+    
+}
+ayuda()
+
+//FUNCIONES 
+// function agregarArticulo() {
+    
+// }
+
+// function eliminarArticulo() {
+    
+// }
+
+// function mostrarCarrito() {
+    
+// }
+
+
+
