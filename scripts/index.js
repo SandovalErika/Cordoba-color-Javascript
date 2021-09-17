3b943e831e940a24c5f1ce135bb8cdbea5c597f5
Erika Sandoval <erikasandoval-09@hotmail.com>
2021-09-17 19:40:11 -0300

    Correcciones para proyecto final

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,3 @@
-// let bienvenido = prompt ("¡Bienvenido a CORDOBA COLOR! ¿Cual es tu nombre?")
-// let pregunta= prompt ("¿Qué producto buscas?")
-
-// function ayuda() {
-//     alert("Hola " + bienvenido  + ". Tenemos "  + pregunta + " a tu disposición" + ". Dirigite a seccion PRODUCTOS para mas información")
-    
-// }
-// ayuda()
-
-// const cartName = document.getElementById('card');
-
-// cartName.innerHTML = 'Carrito'
-
 function evento() {
     $('.masInfo').slideToggle(1000)
 }
